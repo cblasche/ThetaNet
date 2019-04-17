bug in assortativity coefficient computation: wrong mean value again :)

example/parameters.py
@@ -15,8 +15,8 @@ P_k_in = k_in.astype(float) ** (-3)
 P_k_in = P_k_in / np.sum(P_k_in)  # to have sum(P_k)=1
 k_in_mean = np.sum(k_in * P_k_in)  # average value of node degrees
 
-k_out_min = 750
-k_out_max = 2000
+k_out_min = k_in_min
+k_out_max = k_in_max
 k_out = np.arange(k_out_min, k_out_max + 1)
 N_k_out = len(k_out)
 P_k_out = k_out.astype(float) ** (-3)
@@ -34,9 +34,9 @@ rho = .0  # in-out-correlation
 """ Assortativity
 """
 c = 0  # assortativity parameter
-r = 0  # assortativity coefficient
-i_prop = 'in'  # post-synaptic neuron property - 'in' / 'out'
-j_prop = 'out'  # pre-synaptic neuron property
+r = 0.0  # assortativity coefficient
+i_prop = 'out'  # post-synaptic neuron property - 'in' / 'out'
+j_prop = 'in'  # pre-synaptic neuron property
 
 
 """ Node network
@@ -45,8 +45,8 @@ K_in, K_out = None, None
 A = None
 
 K_in, K_out = tn.generate.degree_sequence(k_in, P_k_in, k_out, P_k_out, N, rho)
-# A = tn.generate.configuration_model(K_in, K_out, r, i_prop, j_prop)
-A = tn.generate.chung_lu_model(K_in, K_out, rho, c, i_prop, j_prop)
+A = tn.generate.configuration_model(K_in, K_out, r, i_prop, j_prop)
+# A = tn.generate.chung_lu_model(K_in, K_out, rho, c, i_prop, j_prop)
 
 
 """ Degree network

thetanet/generate/adjacency_matrix/chung_lu.py
@@ -64,7 +64,7 @@ def matrix_from_sequence(K_in, K_out, c=0, i_prop='in', j_prop='out'):
     return A.astype(int)
 
 
-def chung_lu_model(K_in, K_out, rho=0, c=0,  i_prop='in', j_prop='out'):
+def chung_lu_model(K_in, K_out, rho=0, c=0, i_prop='in', j_prop='out'):
     """ Compute an adjacency matrix from in- and out-degree sequences using the
     Chung Lu model.
     Create a
@@ -100,11 +100,11 @@ def chung_lu_model(K_in, K_out, rho=0, c=0,  i_prop='in', j_prop='out'):
 
     if rho == 0:
         np.random.shuffle(K_out)
-        A = matrix_from_sequence(K_in, K_out, c, i_prop='in', j_prop='out')
+        A = matrix_from_sequence(K_in, K_out, c, i_prop=i_prop, j_prop=j_prop)
 
     elif rho > 0:
         K_out.sort()
-        A = matrix_from_sequence(K_in, K_out, c, i_prop='in', j_prop='out')
+        A = matrix_from_sequence(K_in, K_out, c, i_prop=i_prop, j_prop=j_prop)
         for i in range(1, len(K_in)):
             if correlation_from_matrix(A) < rho:
                 break
@@ -112,7 +112,7 @@ def chung_lu_model(K_in, K_out, rho=0, c=0,  i_prop='in', j_prop='out'):
 
     elif rho < 0:
         K_out[::-1].sort()
-        A = matrix_from_sequence(K_in, K_out, c, i_prop='in', j_prop='out')
+        A = matrix_from_sequence(K_in, K_out, c, i_prop=i_prop, j_prop=j_prop)
         for i in range(1, len(K_in)):
             if correlation_from_matrix(A) > rho:
                 break

thetanet/generate/adjacency_matrix/configuration_model.py
@@ -399,19 +399,20 @@ def a_coef_from_matrix(A, i_prop='in', j_prop='out'):
 
     K_in = A.sum(axis=1)
     K_out = A.sum(axis=0)
-    K_mean_edge_in = np.sum(K_in ** 2) / A.sum()
-    K_mean_edge_out = np.sum(K_out ** 2) / A.sum()
+
+    edges = np.argwhere(A > 0)
+    i, j = edges[:, 0], edges[:, 1]
 
     K_i_prop = eval('K_' + i_prop)
     K_j_prop = eval('K_' + j_prop)
-    K_mean_edge_i_prop = eval('K_mean_edge_' + i_prop)
-    K_mean_edge_j_prop = eval('K_mean_edge_' + j_prop)
+    K_mean_edge_i = (A[i, j]*K_i_prop[i]).sum() / A.sum()
+    K_mean_edge_j = (A[i, j]*K_j_prop[j]).sum() / A.sum()
 
     i, j = np.meshgrid(range(N), range(N), indexing='ij')
-    var_i_prop = np.sum(A * (K_i_prop[i] - K_mean_edge_i_prop) ** 2)
-    var_j_prop = np.sum(A * (K_j_prop[j] - K_mean_edge_j_prop) ** 2)
-    cor = np.sum(A * (K_i_prop[i] - K_mean_edge_i_prop) *
-                 (K_j_prop[j] - K_mean_edge_j_prop))
+    var_i_prop = np.sum(A * (K_i_prop[i] - K_mean_edge_i) ** 2)
+    var_j_prop = np.sum(A * (K_j_prop[j] - K_mean_edge_j) ** 2)
+    cor = np.sum(A * (K_i_prop[i] - K_mean_edge_i) *
+                 (K_j_prop[j] - K_mean_edge_j))
     r = cor / (np.sqrt(var_i_prop) * np.sqrt(var_j_prop))
 
     return r

thetanet/generate/assortativity_function.py
@@ -138,15 +138,17 @@ def a_coef_from_a_func2D(a, k_in, P_k_in, k_out, P_k_out, i_prop, j_prop):
     """
 
     # TODO: why can mean over edges be computed like that
-    k_mean_edge_in = P_k_in.dot(k_in ** 2) / P_k_in.dot(k_in)
-    k_mean_edge_out = P_k_out.dot(k_out ** 2) / P_k_out.dot(k_out)
+    k_mean_edge_i_in = P_k_in.dot(k_in ** 2) / P_k_in.dot(k_in)
+    k_mean_edge_i_out = P_k_out.dot(k_out)
+    k_mean_edge_j_in = P_k_in.dot(k_in)
+    k_mean_edge_j_out = P_k_out.dot(k_out ** 2) / P_k_out.dot(k_out)
 
     k_i = eval('k_' + i_prop)
     k_j = eval('k_' + j_prop)
     P_k_i = eval('P_k_' + i_prop)
     P_k_j = eval('P_k_' + j_prop)
-    k_mean_edge_i = eval('k_mean_edge_' + i_prop)
-    k_mean_edge_j = eval('k_mean_edge_' + j_prop)
+    k_mean_edge_i = eval('k_mean_edge_i_' + i_prop)
+    k_mean_edge_j = eval('k_mean_edge_j_' + j_prop)
 
     count_func = a * np.outer(P_k_i, P_k_j)
     mesh_k_i, mesh_k_j = np.meshgrid(k_i, k_j, indexing='ij')
@@ -190,8 +192,12 @@ def a_coef_from_a_func(a, k_in, P_k_in, k_out, P_k_out, i_prop, j_prop):
 
     i_mean_axis = {'in': 1, 'out': 0}
     j_mean_axis = {'in': 3, 'out': 2}
+    mean_prop = {'in': 'out', 'out': 'in'}
+    P_k_i_mean = eval('P_k_' + mean_prop[i_prop])
+    P_k_j_mean = eval('P_k_' + mean_prop[j_prop])
 
-    a = a.sum(axis=(i_mean_axis[i_prop], j_mean_axis[j_prop]))
+    a = np.tensordot(a, P_k_j_mean, (j_mean_axis[j_prop], 0))
+    a = np.tensordot(a, P_k_i_mean, (i_mean_axis[i_prop], 0))
 
     r = a_coef_from_a_func2D(a, k_in, P_k_in, k_out, P_k_out, i_prop, j_prop)
 